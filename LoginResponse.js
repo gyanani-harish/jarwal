3fc84931d509fe0145d69dbb0173069f5d53c16d
Harish Gyanani <gyanani.harish@gmail.com>
2019-12-20 11:07:49 +0530

    Create LoginResponse.js

---
+++ LoginResponse.js
@@ -0,0 +1,49 @@
+{
+  "success": {
+    "secret_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjFiYTdiZDgyOTUwMGVlYTA5N2YxYTZiOWNlYjg5NWQ1MWZjZjhlNzI5ZjU5NmY2ZTkyMzNkMjhlNDliYmU5YmU3NTg1ZGZlNmI2YzM1YjAwIn0.eyJhdWQiOiIzIiwianRpIjoiMWJhN2JkODI5NTAwZWVhMDk3ZjFhNmI5Y2ViODk1ZDUxZmNmOGU3MjlmNTk2ZjZlOTIzM2QyOGU0OWJiZTliZTc1ODVkZmU2YjZjMzViMDAiLCJpYXQiOjE1NzY2NzkxMzgsIm5iZiI6MTU3NjY3OTEzOCwiZXhwIjoxNjA4MzAxNTM4LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.rG5k_7-5R-8saZoyWMC6BxQIQ89HUWjFBMRfK8w7WWpov7bGVFnm9fMflZN-2XDOXj4W4Uk1E_24sU2X27TW84pugVnyEkCn-JpepmRAI32YtclTJs2eHcY-LfbyR947u8za1UJLEQvSWEvv_1wfoiHFBHvffUOwEJe9V2SyMELJytPie1Lmhh7S1DwwV9hwtRUpbj-KNjLIsc5AK7USUgzy1yVmoHOXlDP_ecsm5Tfm-hn9kVTa081kGIiV-mFPzqiStWwfbLE2RAU5pIykqoPDyLBbZ1zh2LGgkBRZsCyfgPBmwRxwTF01gRIjjARTQo9cKNPyZsMFwOYaktdpnJZC5csVLKIh2HwnwuWlo-OdGJ7vK2c_cGibtJzc1hF28mgejsrLrVejsTYPEvNBu171ijkkWBceNVg33gpdXOKGGDR6ldlNnABjmxeBehPd2F16Lo2wX1Vk2de83b9wU4rIuBvYzBj7LxwsP-1KJUvTqYiKwot6_W1JRdgSayTn1E-wMPsv7lMeSAHTSTes99ce0qeT-21m-LCdcs_2WOA4mareJk3omNN-Moe_-uSs5iGUjqcvV40z_eJmK_Zx5AU5LoNd8cCHFDOq8u9HrlNVW6qjbwyUmNhYqoUQF6hMKgrprPKU3WhmAQEmkVYwbwpUJlXgmksV7t6GrvEzUxc",
+    "user": {
+      "id": 1,
+      "employee_code": "XEAM001",
+      "email": "admin@xeam.com",
+      "email_verified_at": null,
+      "device_id": "1jfdgjdfkhjgjkfdgh",
+      "device_type": "android",
+      "forgot_password_token": null,
+      "created_at": "2019-09-10 04:05:25",
+      "updated_at": "2019-12-18 19:55:38",
+      "permissions": [
+        "create-employee",
+        "edit-employee",
+        "delete-employee",
+        "apply-leave",
+        "approve-employee",
+        "manage-masterTable",
+        "approve-leave",
+        "approve-probation",
+        "create-company",
+        "edit-company",
+        "approve-company",
+        "create-project",
+        "edit-project",
+        "approve-project",
+        "view-attendance",
+        "approve-travel",
+        "verify-travel-claim",
+        "import-salaryslip",
+        "view-allsalaryslip",
+        "change-attendance",
+        "it-attendance-approver",
+        "create-task"
+      ],
+      "employee": {
+        "id": 1,
+        "user_id": 1,
+        "fullname": "Super Admin",
+        "profile_picture": "http://erpportaltest.xeamventures.com/public/admin_assets/static_assets/userPic.png",
+        "isactive": 1,
+        "approval_status": "1",
+        "joining_date": "2019-09-10"
+      }
+    }
+  }
+}
